f3a6e20d8ad9dc5e7ab32efe5d204fcce535acff
Petter Holmström <petter.holmstrom@abo.fi>
2011-04-18 11:30:56 +0300

    ListenerList is ready, complet with unit test.

--- Sources/Article3/src/main/java/com/github/peholmst/neo4jvaadindemo/article3/util/ListenerList.java
+++ Sources/Article3/src/main/java/com/github/peholmst/neo4jvaadindemo/article3/util/ListenerList.java
@@ -2,27 +2,47 @@ package com.github.peholmst.neo4jvaadindemo.article3.util;
 
 import java.util.LinkedList;
 
+/**
+ * A list of listeners that can be notified using the Visitor pattern.
+ * 
+ * @author Petter Holmström
+ */
 public class ListenerList<T> implements java.io.Serializable {
 
     private final LinkedList<T> listeners = new LinkedList<T>();
     
+    /**
+     * Adds the specified listener to the list. If the listener is <code>null</code>, nothing happens.
+     */
     public void add(T listener) {
         if (listener != null) {
             listeners.add(listener);
         }
     }
     
+    /**
+     * Removes the specified listener to the list. If the listener is <code>null</code>, or is not
+     * in the list, nothing happens.
+     */
     public void remove(T listener) {
         if (listener != null) {
             listeners.remove(listener);
         }
     }
     
+    /**
+     * Visitor interface (as in the Visitor pattern) for notifying the listeners in the list.
+     */
     public interface ListenerNotifier<T> {
         void notifyListener(T listener);
     }
     
+    /**
+     * Visits (as in the Visitor pattern) all listeners in the list
+     * with the specified notifier (cannot be <code>null</code>).
+     */
     public void notifyListeners(ListenerNotifier<T> notifier) {
+        assert notifier != null : "notifier must not be null";
         LinkedList<T> listenersToNotify = (LinkedList<T>) listeners.clone();
         for (T listener : listenersToNotify) {
             notifier.notifyListener(listener);
--- Sources/Article3/src/test/java/com/github/peholmst/neo4jvaadindemo/article3/util/ListenerListTest.java
+++ Sources/Article3/src/test/java/com/github/peholmst/neo4jvaadindemo/article3/util/ListenerListTest.java
@@ -1,14 +1,124 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.github.peholmst.neo4jvaadindemo.article3.util;
 
+import org.junit.After;
+import org.jmock.Expectations;
+import com.github.peholmst.neo4jvaadindemo.article3.util.ListenerList.ListenerNotifier;
+import org.jmock.Mockery;
+import org.junit.Test;
+import org.junit.Before;
+
 /**
- *
- * @author peholmst
+ * Test-case for {@link ListenerList}.
+ * 
+ * @author Petter Holmström
  */
 public class ListenerListTest {
 
+    Mockery context = new Mockery();
+    ListenerNotifier<String> notifier;
+    /*
+     * We can use Strings as listeners in order to test
+     * the functionality of the list.
+     */
+    private ListenerList<String> listUnderTest;
+
+    @Before
+    public void setUp() {
+        listUnderTest = new ListenerList<String>();
+        notifier = context.mock(ListenerNotifier.class);
+    }
+
+    @After
+    public void tearDown() {
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void addNullListener() {
+        listUnderTest.add(null);
+
+        context.checking(new Expectations() {
+
+            {
+                never(notifier).notifyListener(with(aNull(String.class)));
+            }
+        });
+        listUnderTest.notifyListeners(notifier);
+    }
+
+    @Test
+    public void removeNullListener() {
+        listUnderTest.remove(null);
+        // No exception thrown
+    }
+
+    @Test
+    public void addSingleListener() {
+        listUnderTest.add("Hello");
+
+        context.checking(new Expectations() {
+
+            {
+                oneOf(notifier).notifyListener("Hello");
+            }
+        });
+        listUnderTest.notifyListeners(notifier);
+    }
+
+    @Test
+    public void addSameListenerTwice() {
+        listUnderTest.add("Hello");
+        listUnderTest.add("Hello");
+
+        context.checking(new Expectations() {
+
+            {
+                exactly(2).of(notifier).notifyListener("Hello");                
+            }
+        });
+        listUnderTest.notifyListeners(notifier);
+    }
+    
+    @Test
+    public void addDifferentListeners() {
+        listUnderTest.add("Hello");
+        listUnderTest.add("World");
+
+        context.checking(new Expectations() {
+
+            {
+                oneOf(notifier).notifyListener("Hello");
+                oneOf(notifier).notifyListener("World");
+            }
+        });
+        listUnderTest.notifyListeners(notifier);
+    }
+    
+    @Test
+    public void removeSingleListener() {
+        listUnderTest.add("Hello");
+        listUnderTest.remove("Hello");
+
+        context.checking(new Expectations() {
+
+            {
+                never(notifier).notifyListener(with(aNull(String.class)));
+            }
+        });
+        listUnderTest.notifyListeners(notifier);        
+    }
+    
+    @Test
+    public void testRemoveDuplicateListener() {
+        listUnderTest.add("Hello");
+        listUnderTest.add("Hello");
+        listUnderTest.remove("Hello");
+        context.checking(new Expectations() {
+
+            {
+                oneOf(notifier).notifyListener("Hello");
+            }
+        });
+        listUnderTest.notifyListeners(notifier);
+    }
 }
