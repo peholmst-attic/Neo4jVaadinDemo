c8f83dc4efa0735e65ed2c09b9ab1d3e3f27ed4c
Petter Holmström <petter.holmstrom@abo.fi>
2011-04-20 10:04:53 +0300

    Added javadocs

--- Sources/Article3/src/main/java/com/github/peholmst/neo4jvaadindemo/article3/domain/util/GraphDatabaseServiceProvider.java
+++ Sources/Article3/src/main/java/com/github/peholmst/neo4jvaadindemo/article3/domain/util/GraphDatabaseServiceProvider.java
@@ -2,8 +2,18 @@ package com.github.peholmst.neo4jvaadindemo.article3.domain.util;
 
 import org.neo4j.graphdb.GraphDatabaseService;
 
+/**
+ * The {@link GraphDatabaseService} interface is not serializable. The purpose
+ * of this provider interface is to make it possible for serializable classes
+ * to contain a serializable reference to the Neo4j graph database.
+ * 
+ * @author Petter Holmström
+ */
 public interface GraphDatabaseServiceProvider extends java.io.Serializable {
 
+    /**
+     * Returns the Neo4j graph database service.
+     */
     GraphDatabaseService getGraphDb();
     
 }
